277b19b970273ed1122853afe67a19ab66d67d0f
kahahope <kahahope@gmail.com>
2018-03-19 21:45:22 +0300

    Update quadratic.rb

--- Lesson_1/quadratic.rb
+++ Lesson_1/quadratic.rb
@@ -5,15 +5,15 @@ b = gets.to_f
 puts "Укажите значение c"
 c = gets.to_f
 
-d = b**2 - 4 * a * c
+d = b**2 - (4 * a * c)
 
 if d == 0
-  x = -b / 2 * a
+  x = -b / (2 * a)
   puts "Уравнение имеет один корень, который равен #{x}"
 elsif d > 0
   root = Math.sqrt(d)
-  x1 = (-b + root) / 2 * a
-  x2 = (-b - root) / 2 * a
+  x1 = (-b + root) / (2 * a)
+  x2 = (-b - root) / (2 * a)
   puts "Уравнение имеет два корня, которые равны #{x1} и #{x2} соответственно"
 else
   puts "Корней нет"
