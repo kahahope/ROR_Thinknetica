4eceaaa425ea2e7cc20fc355da5cd645cb6416cd
kahahope <kahahope@gmail.com>
2018-03-19 15:39:33 +0300

    Update triangle_area.rb

--- Lesson_1/triangle_area.rb
+++ Lesson_1/triangle_area.rb
@@ -1,8 +1,8 @@
 puts "Укажите основание треугольника"
-base = gets.chomp.to_f
+base = gets.to_f
 
 puts "Укажите высоту треугольника"
-hight = gets.chomp.to_f
+hight = gets.to_f
 
 area = 0.5 * a * h
 puts "Площадь треугольника составляет #{area}"
